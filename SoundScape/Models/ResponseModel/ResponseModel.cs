35245b1ad9a7e65bd2cd7b306178ee61bc6f43fa
Marcelo1080p <juniooor.2015@gmail.com>
2024-09-09 19:14:52 -0300

    class generica criada: "ResponseModel"

---
+++ SoundScape/Models/ResponseModel/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SoundScape.Models.ResponseModel
+{
+    public class ResponseModel<T>
+    {
+        public T? Data { get; set; }
+        public string Menssage { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;
+    }
+}
